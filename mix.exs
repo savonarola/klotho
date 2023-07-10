a42796fdb253711b524284161cd0f7d4826b7a4d
Ilya Averyanov <av@rubybox.dev>
2023-07-10 10:39:45 +0300

    loosen version requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Klotho.MixProject do
     [
       app: :klotho,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
