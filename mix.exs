28df9049dfbb2d64a66bf191aeaaf31f54cb9a7b
Ilya Averyanov <av@rubybox.dev>
2023-07-16 16:12:36 +0300

    set inititial version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Klotho.MixProject do
     [
       app: :klotho,
       description: description(),
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() in [:dev, :test],
       deps: deps(),
